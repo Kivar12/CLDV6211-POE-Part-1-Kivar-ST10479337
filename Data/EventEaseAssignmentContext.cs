1e053bb2d15b371c2f6aa797f5227c6c9c6fb49d
Kivar12 <ST10479337@vcconnect.edu.za>
2026-04-15 23:36:00 +0200

    Add files via upload

---
+++ Data/EventEaseAssignmentContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EventEase.Models;
+
+namespace EventEaseAssignment.Data
+{
+    public class EventEaseAssignmentContext : DbContext
+    {
+        public EventEaseAssignmentContext (DbContextOptions<EventEaseAssignmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EventEase.Models.Venues> Venues { get; set; } = default!;
+        public DbSet<EventEase.Models.Bookings> Bookings { get; set; } = default!;
+        public DbSet<EventEase.Models.Events> Events { get; set; } = default!;
+    }
+}
